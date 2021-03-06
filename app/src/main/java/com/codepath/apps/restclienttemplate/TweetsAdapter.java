960fba4e65e995d3ad545e822e8094fc755f1569
andrey548 <andreyverh@icloud.com>
2021-03-05 23:15:15 -0500

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -0,0 +1,89 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.util.List;
+
+public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder> {
+
+    Context context;
+    List<Tweet> tweets;
+
+    //Pass in the context and list of tweets
+    public TweetsAdapter(Context context, List<Tweet> tweets) {
+        this.context = context;
+        this.tweets = tweets;
+    }
+
+    //for each row, inflate the layout
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_tweet, parent ,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Get the data at position
+        Tweet tweet = tweets.get(position);
+        // Bind the tweet with view holder
+        holder.bind(tweet);
+    }
+
+    @Override
+    public int getItemCount() {
+        return tweets.size();
+    }
+
+    //Clear
+    public void clear() {
+        tweets.clear();
+        notifyDataSetChanged();
+    }
+
+    //Add
+    public void addAll(List<Tweet> tweetList) {
+        tweets.addAll(tweetList);
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView ivProfileImage;
+        TextView tvBody;
+        TextView tvScreenName;
+        TextView tvTime;
+        TextView tvRealName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
+            tvBody = itemView.findViewById(R.id.tvBody);
+            tvScreenName = itemView.findViewById(R.id.tvScreenName);
+            tvTime = itemView.findViewById(R.id.tvTime);
+            tvRealName = itemView.findViewById(R.id.tvRealName);
+        }
+
+        public void bind(Tweet tweet) {
+            tvBody.setText(tweet.body);
+            //tvScreenName.setText(tweet.user.screenName);
+            //tvTime.setText(tweet.time);
+            Glide.with(context).load(tweet.user.profileImageUrl).circleCrop().into(ivProfileImage);
+            tvTime.setText(tweet.getFormattedTimestamp(tweet.time));
+            tvScreenName.setText(tweet.getFormattedHandle(tweet.user.screenName));
+            tvRealName.setText(tweet.user.name);
+        }
+    }
+}
